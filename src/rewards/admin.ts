4777a13ce0873d21daccf98c75de10113e497427
lujain al mansoori <lmansoor@andrew.cmu.edu>
2023-08-24 10:39:19 +0300

    Translated imports, line 1-3

--- src/rewards/admin.ts
+++ src/rewards/admin.ts
@@ -1,112 +1,79 @@
 import * as plugins from '../plugins';
 import * as db from '../database';
 import * as utils from '../utils';
+//
+const rewards = module.exports;
 
+rewards.save = async function (data) {
+    async function save(data) {
+        if (!Object.keys(data.rewards).length) {
+            return;
+        }
+        const rewardsData = data.rewards;
+        delete data.rewards;
+        if (!parseInt(data.id, 10)) {
+            data.id = await db.incrObjectField('global', 'rewards:id');
+        }
+        await rewards.delete(data);
+        await db.setAdd('rewards:list', data.id);
+        await db.setObject(`rewards:id:${data.id}`, data);
+        await db.setObject(`rewards:id:${data.id}:rewards`, rewardsData);
+    }
 
-type RewardData = {
-    id?: string | number;
-    rewards: { [key: string]: string };
-    condition: string;
-    disabled?: boolean | string;
+    await Promise.all(data.map(data => save(data)));
+    await saveConditions(data);
+    return data;
 };
 
-interface GetReturnType {
-    // Random types
-    active: boolean;
-    conditions: string;
-    conditionals: boolean;
-    rewards: string;
-}
+rewards.delete = async function (data) {
+    await Promise.all([
+        db.setRemove('rewards:list', data.id),
+        db.delete(`rewards:id:${data.id}`),
+        db.delete(`rewards:id:${data.id}:rewards`),
+    ]);
+};
 
-type RewardsType = { [key: string]: string };
-
-type SaveInput = RewardData[];
-
-async function getActiveRewards(): Promise<RewardData[]> {
-    // Nested function with types
-    async function load(id: string | number): Promise<RewardData | null> {
-        const [main, rewards] = await Promise.all([
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            db.getObject(`rewards:id:${id}`),
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            db.getObject(`rewards:id:${id}:rewards`),
-        ]) as [RewardData | null, RewardsType | null];
-
-        if (main) {
-            main.disabled = main.disabled === 'true';
-            main.rewards = rewards || {};
-            return main;
-        }
-        return null;
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const rewardsList: (string | number)[] = await db.getSetMembers('rewards:list') as string[];
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const rewardData: (RewardData | null)[] = await Promise.all(rewardsList.map(id => load(id)));
-    return rewardData.filter(Boolean);
-}
-export const rewards = {
-    save: async function (data: SaveInput): Promise<RewardData[]> {
-        const save = async (data: RewardData): Promise<RewardData> => {
-            if (!Object.keys(data.rewards).length) {
-                return data; // return data as it is if no rewards are present
-            }
-            const rewardsData = data.rewards;
-            delete data.rewards;
-            if (!parseInt(data.id as string, 10)) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                data.id = await db.incrObjectField('global', 'rewards:id') as number;
-            }
-            await rewards.delete(data);
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await db.setAdd('rewards:list', data.id as string);
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await db.setObject(`rewards:id:${data.id}`, data);
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await db.setObject(`rewards:id:${data.id}:rewards`, rewardsData);
-            // CHECK THIS
-            data.rewards = rewardsData; // add back the rewards to data
-            return data;
-        };
-        // CHECK THIS
-        const savedData = await Promise.all(data.map(save));
-        await rewards.saveConditions(data);
-        return savedData;
-    },
-    delete: async (data: RewardData): Promise<void> => {
-        await Promise.all([
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            db.setRemove('rewards:list', data.id) as string[],
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            db.delete(`rewards:id:${data.id}`) as string[],
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            db.delete(`rewards:id:${data.id}:rewards`) as string[],
-        ]);
-    },
-
-    get: async (): Promise<GetReturnType> => utils.promiseParallel({
+rewards.get = async function () {
+    return await utils.promiseParallel({
         active: getActiveRewards(),
         conditions: plugins.hooks.fire('filter:rewards.conditions', []),
         conditionals: plugins.hooks.fire('filter:rewards.conditionals', []),
         rewards: plugins.hooks.fire('filter:rewards.rewards', []),
-    }),
-    saveConditions: async function (data: RewardData[]): Promise<void> {
-        const rewardsPerCondition: { [key: string]: (string | number)[] } = {};
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await db.delete('conditions:active') as string[];
-        const conditions: string[] = [];
-
-        data.forEach((reward) => {
-            conditions.push(reward.condition);
-            rewardsPerCondition[reward.condition] = rewardsPerCondition[reward.condition] || [];
-            rewardsPerCondition[reward.condition].push(reward.id);
-        });
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await db.setAdd('conditions:active', conditions) as string[];
-        await Promise.all(
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            Object.keys(rewardsPerCondition).map(c => db.setAdd(`condition:${c}:rewards`, rewardsPerCondition[c]) as string[])
-        );
-    },
+    });
 };
-export default rewards;
+
+async function saveConditions(data) {
+    const rewardsPerCondition = {};
+    await db.delete('conditions:active');
+    const conditions = [];
+
+    data.forEach((reward) => {
+        conditions.push(reward.condition);
+        rewardsPerCondition[reward.condition] = rewardsPerCondition[reward.condition] || [];
+        rewardsPerCondition[reward.condition].push(reward.id);
+    });
+
+    await db.setAdd('conditions:active', conditions);
+
+    await Promise.all(Object.keys(rewardsPerCondition).map(c => db.setAdd(`condition:${c}:rewards`, rewardsPerCondition[c])));
+}
+
+async function getActiveRewards() {
+    async function load(id) {
+        const [main, rewards] = await Promise.all([
+            db.getObject(`rewards:id:${id}`),
+            db.getObject(`rewards:id:${id}:rewards`),
+        ]);
+        if (main) {
+            main.disabled = main.disabled === 'true';
+            main.rewards = rewards;
+        }
+        return main;
+    }
+
+    const rewardsList = await db.getSetMembers('rewards:list');
+    const rewardData = await Promise.all(rewardsList.map(id => load(id)));
+    return rewardData.filter(Boolean);
+}
+
+require('../promisify')(rewards);
